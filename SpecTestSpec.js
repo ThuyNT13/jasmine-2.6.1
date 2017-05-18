4d84005930ed82d778508cc9e65f621044da3a75
Thuy <thuy@thuy.life>
2017-05-18 13:23:33 -0500

    typo

--- SpecTestSpec.js
+++ SpecTestSpec.js
@@ -1,6 +1,6 @@
 describe('testHello()', function() {
   describe('returns a hello', function() {
-    it('Hello World!', functin() {
+    it('Hello World!', function() {
       expect(testHello()).toEqual('Hello World!');
     });
   });
